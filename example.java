0496a719a9eefc548522684a28f1e1348d487f45
Daviskedy <daviskedy50@gmail.com>
2024-09-25 13:14:26 +0300

    Add files via upload

---
+++ example.java
@@ -0,0 +1,24 @@
+//outputs
+
+public class example {
+    public static void main(String[] args) {
+        // Arithmetic operators
+        int a = 10;
+        int b = 5;
+        // Addition
+        int sum = a + b;
+        // Substraction
+        int difference = a - b;
+        // multiplication
+        int product = a * b;
+        // division
+        int quotient = a / b;
+        // Modulus
+        int remainder = a % b;
+        System.out.println("sum: " + sum);
+        System.out.println("difference: " + difference);
+        System.out.println("product: " + product);
+        System.out.println("quotient: " + quotient);
+        System.out.println("remainder: " + remainder);
+    }
+}
